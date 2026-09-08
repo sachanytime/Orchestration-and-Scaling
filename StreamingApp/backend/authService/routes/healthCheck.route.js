2fd6b317c9e9ffc2f3c41879fb2bf18395cc373e
sachanytime <sachanytime@gmail.com>
2026-09-08 23:57:02 +0530

    Remove all repository files

--- StreamingApp/backend/authService/routes/healthCheck.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const routes = express.Router()
-
-const healthCheckController = require('../controllers/healthCheck.controller')
-
-routes.get('/', healthCheckController.healthCheck)
-
-
-module.exports = routes
--- StreamingApp/backend/streamingService/controllers/health.controller.js
+++
@@ -1,9 +0,0 @@
-const healthCheck = async (req,res) =>{
-    try{
-        res.json({msg: 'OK'})
-    }catch(err){
-        res.json({msg: 'Something went wrong.'})
-    }
-}
-
-module.exports = { healthCheck }
